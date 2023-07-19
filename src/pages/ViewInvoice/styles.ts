b98c775680d446c291141838cf4dc431742fb7e3
sekayn <129896992+sekayn@users.noreply.github.com>
2023-07-19 10:54:16 +0100

    invoice app files

--- src/pages/ViewInvoice/styles.ts
+++ src/pages/ViewInvoice/styles.ts
@@ -21,7 +21,8 @@ const viewInvoiceContent = css({
     borderRadius: "8px",
     backgroundColor: "#FFF",
     boxShadow: "0px 10px 10px - 10px rgba(72, 84, 159, 0.10)"
-}
+})
+
 export const styles = {
     viewInvoiceBanner,
     viewInvoice,
